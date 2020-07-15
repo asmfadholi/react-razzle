7de110374cd2a234efd0f4143f612d96f00e5163
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-07-16 01:49:03 +0700

    chore: add razzle-heroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const isHeroku = require("is-heroku");
+const entries = require("object.entries");
 
 module.exports = {
     plugins: ['scss', 'workbox'],
@@ -18,6 +20,24 @@ module.exports = {
           'services': path.resolve(__dirname, 'src/services'),
           'pages': path.resolve(__dirname, 'src/pages'),
       }
+
+      const isDefinePlugin = plugin => plugin.constructor.name === "DefinePlugin"
+      const indexDefinePlugin = config.plugins.findIndex(isDefinePlugin)
+
+      if (indexDefinePlugin < 0) {
+        console.warn("Couldn't setup razzle-heroku, no DefinePlugin...")
+        return config
+      }
+
+      const {definitions} = config.plugins[indexDefinePlugin]
+      const newDefs = Object.assign({}, definitions);
+
+      if (isHeroku) {
+        delete newDefs["process.env.PORT"]
+        newDefs["process.env.RAZZLE_PUBLIC_DIR"] = '"/app/build/public"'
+      }
+
+      config.plugins[indexDefinePlugin] = new webpack.DefinePlugin(newDefs)
   
       return config;
     }
